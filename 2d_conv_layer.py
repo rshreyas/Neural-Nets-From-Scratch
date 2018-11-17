00032d07a23026ebed5f798ec3453cb57c963380
Shreyas Ramesh <shreyas2@vt.edu>
2018-11-17 16:47:04 -0500

    Stacked 2D CNNs now complete, with ReLu and MaxPool2D. Convolution Viewer also complete.

--- 2d_conv_layer.py
+++ 2d_conv_layer.py
@@ -3,6 +3,7 @@ import skimage
 from pprint import pprint
 import skimage.data
 from skimage.viewer import ImageViewer
+from skimage.transform import resize
 
 class Conv2D():
     def __init__(self, num_kernels=4, kernel_shape=[3,3], padding='valid', step_size=1):
@@ -19,7 +20,7 @@ class Conv2D():
         return img
 
 
-    def _init_filters(self, standard_norm_init=True):
+    def _init_filters(self, standard_norm_init=True, num_channels=1):
         num_kernels = self.num_kernels
         input_shape = self.kernel_shape
         layer_filters = None
@@ -30,11 +31,20 @@ class Conv2D():
 
         if not standard_norm_init:
             # Initialize filters to 0s
-            layer_filters = np.zeros(num_kernels, input_shape[0], input_shape[1])
+            if num_channels > 1:
+                layer_filters = np.zeros(num_kernels, input_shape[0], input_shape[1], num_channels)
+            else:
+                layer_filters = np.zeros(num_kernels, input_shape[0], input_shape[1])
         else:
-             layer_filters = np.random.standard_normal(size=(num_kernels,
-                                                            input_shape[0],
-                                                            input_shape[1]))
+            if num_channels > 1:
+                layer_filters = np.random.standard_normal(size=(num_kernels,
+                                                                input_shape[0],
+                                                                input_shape[1],
+                                                                num_channels))
+            else:
+                layer_filters = np.random.standard_normal(size=(num_kernels,
+                                                                input_shape[0],
+                                                                input_shape[1]))
 
         self.layer_filters = layer_filters
 
@@ -43,9 +53,9 @@ class Conv2D():
         if len(img.shape) > 2 or len(layer_filters.shape) > 3:
             # At this point, either image or filters has been judged to be non-default
             if channel_first:
-                assert(len(img.shape[0]) == len(layer_filters.shape[0]))
+                assert(img.shape[0] == layer_filters.shape[0])
             else:
-                assert(len(img.shape[-1]) == len(layer_filters.shape[-1]))
+                assert(img.shape[-1] == layer_filters.shape[-1])
         return True
 
 
@@ -70,12 +80,16 @@ class Conv2D():
 
 
     def get_layer_feature_maps(self, img_source=None, to_grayscale=True, channel_first=False):
-        if img_source:
-            img = self._process_input_image(img_source, to_grayscale)
+        if img_source is not None:
+            img = img_source
+            self._init_filters(standard_norm_init=True, num_channels=img_source.shape[-1])
         else:
             img = self._process_input_image(to_grayscale)
+            if to_grayscale:
+                self._init_filters(standard_norm_init=True, num_channels=1)
+            else:
+                self._init_filters(standard_norm_init=True, num_channels=3)
 
-        self._init_filters()
         layer_filters = self.layer_filters
 
         if self._check_channel_dims(img, layer_filters):
@@ -120,12 +134,47 @@ def relu(feature_maps):
     return relu_out
 
 
+def maxpool_2d(feature_maps, size=2, stride=2):
+    maxpool_out = np.empty(((feature_maps.shape[0] - size + 1) / stride,
+                            (feature_maps.shape[1] - size + 1) / stride,
+                            feature_maps.shape[-1]))
+
+    for map_idx in range(feature_maps.shape[-1]):
+        row_pool_idx = 0
+        for row in range(0, feature_maps.shape[0] - size - 1, stride):
+            col_pool_idx = 0
+            for col in range(0, feature_maps.shape[1] - size - 1, stride):
+                maxpool_out[row_pool_idx, col_pool_idx, map_idx] = np.max(feature_maps[row: row + size, col: col + size, map_idx])
+                col_pool_idx += 1
+            row_pool_idx += 1
+    return maxpool_out
+
+
+def display_conv_relu_pool_block(feature_maps, relu_out, maxpool_out):
+    img_list = []
+    for i in range(feature_maps.shape[-1]):
+        img = np.concatenate((feature_maps[:,:,i],
+                              relu_out[:,:,i],
+                              resize(maxpool_out[:,:,i],
+                                     (relu_out.shape[0],
+                                     relu_out.shape[1]))),
+                              axis=1)
+        img_list.append(img)
+    img_final = np.concatenate(([i for i in img_list]), axis=0)
+
+    viewer = ImageViewer(img_final)
+    viewer.show()
+
+
 if __name__ == "__main__":
     c1 = Conv2D()
-    #c1.input_image(to_grayscale=True)
-    feature_maps = c1.get_layer_feature_maps()
-    relu_out = relu(feature_maps)
-    for i in range(feature_maps.shape[-1]):
-        img = np.concatenate((feature_maps[:,:,i], relu_out[:,:,i]), axis=1)
-        viewer = ImageViewer(img)
-        viewer.show()
+    feature_maps_1 = c1.get_layer_feature_maps()
+    relu_out_1 = relu(feature_maps_1)
+    maxpool_out_1 = maxpool_2d(relu_out_1)
+    display_conv_relu_pool_block(feature_maps_1, relu_out_1, maxpool_out_1)
+
+    c2 = Conv2D()
+    feature_maps_2 = c2.get_layer_feature_maps(img_source=maxpool_out_1)
+    relu_out_2 = relu(feature_maps_2)
+    maxpool_out_2 = maxpool_2d(relu_out_2)
+    display_conv_relu_pool_block(feature_maps_2, relu_out_2, maxpool_out_2)
